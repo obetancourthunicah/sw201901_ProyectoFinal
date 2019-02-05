de266eaa06a030b676e209d8f8837b73734f8e28
Orlando J Betancourth <orlando.betancourth@gmail.com>
2019-02-04 18:48:13 -0600

    Temporary File Store of JSON

--- obtbkend/routes/api/things.js
+++ obtbkend/routes/api/things.js
@@ -1,14 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var fileModel = require('./jsonmodel');
+var data = null; // temporary store
+
+var bigThingTp = {
+  'desc':'',
+  'fcIng':null,
+  'author':'',
+  'due':null,
+  'done':false,
+  'type': 'small' // big
+};
 //obtenerOneBigThing
 router.get('/', function( req, res, next) {
-  res.json({"msg":"ok"});
+  if(!data){
+    fileModel.read(function(err, filedata){
+      if(err){
+        console.log(err);
+        data = [];
+        return res.status(500).json({'error':'Error al Obtener Data'});
+      }
+      data = JSON.parse(filedata);
+      return res.status(200).json(data);
+    });
+  } else {
+    return res.status(200).json(data);
+  }
 });// get /
 
 router.post('/new', function(req, res, next){
-  var _thingsData = req.body;
-  console.log(_thingsData);
-  res.json({"msg":"ok"});
+  var _thingsData = Object.assign({} , bigThingTp, req.body);
+  _thingsData.fcIng = new Date;
+  _thingsData.due = new Date(_thingsData.fcIng.setDate(_thingsData.fcIng.getDate()+3));
+  if(!data){
+    data = [];
+  }
+  data.push(_thingsData);
+  fileModel.write(data, function(err){
+    if(err){
+      console.log(err);
+      return res.status(500).json({ 'error': 'Error al Obtener Data' });
+    }
+    return res.status(200).json(_thingsData);
+  });
 });// nuevo bigThing
 
 router.put('/done/:thingId', function(req, res, next){
