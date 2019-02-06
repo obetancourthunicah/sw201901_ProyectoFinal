cde005bca33f98d70b7f2aeb7253b2695c7ef513
Orlando J Betancourth <orlando.betancourth@gmail.com>
2019-02-05 18:45:53 -0600

    Agregando ID con uuid y finalizando implementacion de PUT y DELETE

--- obtbkend/routes/api/things.js
+++ obtbkend/routes/api/things.js
@@ -1,9 +1,13 @@
+var uuidv4 = require('uuid/v4');
 var express = require('express');
 var router = express.Router();
+
+
 var fileModel = require('./jsonmodel');
 var data = null; // temporary store
 
 var bigThingTp = {
+  '_id':'',
   'desc':'',
   'fcIng':null,
   'author':'',
@@ -35,7 +39,7 @@ router.post('/new', function(req, res, next){
   dateD.setDate(dateT.getDate()+ 3);
   _thingsData.fcIng = dateT;
   _thingsData.due = dateD;
-
+  _thingsData._id = uuidv4();
   if(!data){
     data = [];
   }
@@ -51,8 +55,59 @@ router.post('/new', function(req, res, next){
 
 router.put('/done/:thingId', function(req, res, next){
   var _thingId = req.params.thingId;
-  console.log(_thingId);
-  res.json({"msg":"ok"});
+  var _thingUpds = req.body;
+  var _thingUpdated = null;
+  var newData = data.map(
+    function(doc, i){
+      if (doc._id == _thingId){
+        _thingUpdated = Object.assign(
+          {},
+          doc,
+          {"done":true},
+          _thingUpds
+          );
+        return _thingUpdated;
+      }
+      return doc;
+    }
+  );// end map
+  data = newData;
+  fileModel.write(data, function (err) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ 'error': 'Error al Guardar Data' });
+    }
+    return res.status(200).json(_thingUpdated);
+  });
 });// Set A Thing as Done
+
+
+router.delete('/delete/:thingId', function(req, res, next){
+  var _thingId = req.params.thingId;
+  var newData = data.filter(
+    function (doc, i) {
+      if (doc._id == _thingId) {
+        return false;
+      }
+      return true;
+    }
+  );// end map
+  data = newData;
+  fileModel.write(data, function (err) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ 'error': 'Error al Guardar Data' });
+    }
+    return res.status(200).json({"delete": _thingId});
+  });
+}); // end delete
+
+fileModel.read(function(err , filedata){
+  if(err){
+    console.log(err);
+  } else{
+    data = JSON.parse(filedata);
+  }
+});
 
 module.exports = router;
