cee34292b9b0069004d86b1e0087d0c30d4e4fe9
Orlando J Betancourth Alvarenga <obetancourth@unicah.edu>
2019-04-04 18:44:38 -0600

    Release 1.0

---
+++ docs/Build_For_Prd.md
@@ -0,0 +1,17 @@
+# Como implementar en Producción
+
+## Requisitos
+1. Server (DigitalOcean, Vultr, AWS S, Heroku)
+2. HostName (Dominio) 
+3. node.js
+4. git
+5. mongodb
+6. pm2  administrador de procesos de node.js
+
+## Build 
+basckend, frontend 
+(*Arquitectura es fe -> be)
+
+
+
+Hosting ( Shared Hosting | Vps o Containers)
---
+++ obtbkend/public/precache-manifest.32fbc776734c9e52e0302c911e9ff4fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "39a14c417be7120a495a",
+    "url": "/static/js/main.a72095ef.chunk.js"
+  },
+  {
+    "revision": "ca166664917ac4c4774f",
+    "url": "/static/js/2.e918580e.chunk.js"
+  },
+  {
+    "revision": "39a14c417be7120a495a",
+    "url": "/static/css/main.fd6e0f46.chunk.css"
+  },
+  {
+    "revision": "0bd0ce232d9d926e7fe99bff129717b1",
+    "url": "/index.html"
+  }
+];
